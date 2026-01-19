26b5c7fb55dde973fc341d5bfa0db8477bf8a8d5
Johanna Götz <mt241006@fhstp.ac.at>
2026-01-19 21:49:02 +0100

    Particle System funkt wirklich & Highscore beginn
    Yaaayyy

---
+++ Horrorcorn/Assets/Project/_Scripts/Highscores.cs
@@ -0,0 +1,68 @@
+/*using System.Collections.Generic;
+using System.IO;
+using Unity.Plastic.Newtonsoft.Json;
+using UnityEngine;
+
+public class HighScores
+{
+    public class Entry
+    {
+        public string Name;
+        public int Score;
+
+        public override string ToString()
+        {
+            return $"{Name}: {Score}";
+        }
+    }
+    
+    private List<Entry> entries = new List<Entry>();
+    
+    private string fileName = "highscores.json";
+
+    public HighScores()
+    {
+        Load();
+    }
+
+    private void Save()
+    {
+        string json = JsonConvert.SerializeObject(entries);
+        string path = Path.Combine(Application.persistentDataPath, fileName);
+        File.WriteAllText(path, json); //immer path zuerst sonst wird ins json der path gespeichert
+    }
+
+    private void Load()
+    {
+        string path = Path.Combine(Application.persistentDataPath, fileName);
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            entries = JsonConvert.DeserializeObject<List<Entry>>(json);
+        }
+        
+    }
+    
+    public void AddEntry(string name, int score)
+    {
+        var entry = new Entry(){Name = name, Score = score};
+        entries.Add(entry);
+        Sort();
+        Save();
+    }
+
+    private void Sort()
+    {
+        entries.Sort((entryA, entryB) => { return entryA.Score.CompareTo(entryB.Score); });
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        foreach (Entry entry in entries)
+        {
+            result += entry.ToString() + "\n";
+        }
+        return result;
+    }
+}*/
